3029c47011224c2e1a8afbda16e75735fe1e44c2
Josh Talley <jtdev84@gmail.com>
2022-10-27 14:37:37 +0100

    Create Main, update CSS

--- components/Main.js
+++ components/Main.js
@@ -4,7 +4,17 @@ import ReactDOM from "react-dom"
 
 function Main() {
     return (
-        <h1>Main goes here</h1>
+    <div className="main-div">
+        <h1>React</h1>
+        <ul className="main-list">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+        <img className="bg-img" src="images/Group.png"></img>
+    </div>
     )
 }
 
