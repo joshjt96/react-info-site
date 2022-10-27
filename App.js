b9b415aef779a9d27d056635485a68c82ace24f1
Josh Talley <jtdev84@gmail.com>
2022-10-27 12:13:32 +0100

    First commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+// import { createRoot } from 'react-dom/client';
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+export default App
---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+// import { createRoot } from 'react-dom/client';
+
+function Navbar() {
+    return (
+        <h1>Navbar goes here</h1>
+    )
+}
+
+export default Navbar
